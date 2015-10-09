81159469a83bf2287df3034324a6602841e60645
AtheistJesus <atheistjesus@users.noreply.github.com>
2015-10-08 21:04:37 -0700

    Added screenhsot button onto the actionbar. It saves the screenshot fine, but HTC's Gallery is unable to see it unless a media re-scan is preformed.

--- app/src/main/java/kyleavalani/com/myreps/MainActivity.java
+++ app/src/main/java/kyleavalani/com/myreps/MainActivity.java
@@ -1,10 +1,22 @@
 package kyleavalani.com.myreps;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +47,70 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.save) {
+            //Intent i = new Intent(this,SavingActivity.class);
+            //startActivity(i);
+
+
+
+
+            takeScreenshot();
+
+
+
+
+
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+
+    private void takeScreenshot() {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            //File imageFile = new File(mPath);
+            //File imageFile = new File(Environment.getExternalStorageDirectory() + File.separator + "/Pictures/" + "/Screenshots/" + "test.jpg");
+            File imageFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + File.separator +  "/Screenshots/" + "test.jpg");
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+            e.printStackTrace();
+        }
+    }
+
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
+    }
+
+
+
+
 }
